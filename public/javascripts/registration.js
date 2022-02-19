1e6ba97c6b24bf873ccaa618cdf2d402cca421e7
Rob Fentress <learn@vt.edu>
2022-02-18 21:50:00 -0500

    nightly

---
+++ public/javascripts/registration.js
@@ -0,0 +1,110 @@
+
+$(document).ready(function () {
+
+    var secureFlag = true;
+    var f = navigator.userAgent.search("Firefox");
+    var c = navigator.userAgent.search("Chrome");
+    var ie = navigator.userAgent.search("MSIE") + (navigator.userAgent.search(/Trident/)); // MSIE for older ie and Trident for newer IE such as IE 11
+    var ie9 = navigator.userAgent.search("MSIE 9.0");
+
+    var zoomlvl = 100;
+    var cookie_name = "zoom_level";
+    var index = document.cookie.indexOf(cookie_name);
+
+    // write cookies
+    function write_cookie_for_zoom(zoomLevel) {
+        $.cookie(cookie_name, zoomLevel, { path: '/', expires: 3650, secure: secureFlag });
+    }
+
+    function zoom(zoomlvl) {
+        if (f > -1) { // for firefox, use css3: transform:scale
+            $("body").css({
+                "-moz-transform": "scale(" + zoomlvl / 100 + ")",
+                "-moz-transform-origin": "0 0"
+            });
+            var viewPortWidth = $(window).width();
+            var currMainContentWrapperWidth = viewPortWidth * (zoomlvl / 100);
+            var mainContentWidthDiff = (currMainContentWrapperWidth - viewPortWidth) / 2;
+            if (mainContentWidthDiff > 50) { /* zoom in */
+                $("body").css("left", "-" + mainContentWidthDiff + "px");
+            } else if (mainContentWidthDiff < 50) {  /* zoom out */
+                $("body").css("left", mainContentWidthDiff + "px");
+            }
+
+        } else if (ie9 > -1) { // for ie9
+            $("body").css({
+                "transform": "scale(" + zoomlvl / 100 + ")",
+                "transform-origin": "0 0",
+                "-ms-transform": "scale(" + zoomlvl / 100 + ")",
+                "-ms-transform-origin": "0 0"
+            });
+
+        } else {  // for other browers, use zooming
+            $("body").css("zoom", zoomlvl + "%");
+            if (ie > -1) {  // for ie, re-position the div to center
+                $("body").css("width", "100%");
+                // center the viewport in the center if there is scroll bar in the window.
+                var newPosition = ($(document).width() - $(window).width()) / 2;
+                window.scrollTo(newPosition, 0);
+            }
+        }
+        write_cookie_for_zoom(zoomlvl);
+    }
+
+    // zoom back
+    $("#back_to_original_page").click(function () {
+        zoomlvl = 100;
+        zoom(zoomlvl);
+    });
+
+    // zoom in the page
+    $("#zoom_in_page").click(function () {
+        // max zoom level is 150%
+        if (zoomlvl >= 150) {
+            return;
+        }
+        zoomlvl += 10;
+        zoom(zoomlvl);
+    });
+
+    // zoom out the page
+    $("#zoom_out_page").click(function () {
+        // min zoom level is 70%
+        if (zoomlvl <= 70) {
+            return;
+        }
+        zoomlvl -= 10;
+        zoom(zoomlvl);
+    });
+
+    $('.password-toggle').on('click', function (evt) {
+        var fieldSel = $(this).attr('data-for');
+        $(fieldSel).each(function () {
+            if (this.type === "password") {
+                this.type = "text";
+            } else {
+                this.type = "password";
+            }
+        });
+    });
+
+    $('.question-toggle').on('change', function (evt) {
+        var questionId = $(this).attr('data-for');
+        var value = $(this).attr('data-option');
+
+        var question = $(`#${questionId}`)[0];
+        var label = $(`[for="${questionId}"]`);
+        console.log(label);
+
+        if ($(this).val() === value) {
+            question.disabled = false;
+            question.required = true;
+            $(label).removeClass("text-muted");
+        } else {
+            question.disabled = true;
+            question.required = false;
+            $(label).addClass("text-muted");
+        }
+    });
+
+}); 
--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -10,15 +10,35 @@
     var forms = document.querySelectorAll('.needs-validation');
     const pw1 = "password";
     const pw2 = "retype";
+    const username = document.querySelector(`#username`);
+    const usernameFeedback = document.querySelector(`#usernameInvalid`);
     const password = document.querySelector(`#${pw1}`);
     const retype = document.querySelector(`#${pw2}`);
     const email = document.querySelector(`#email`);
+    const ssn = document.querySelector(`#ssn`);
+    const dob = document.querySelector(`#dob`);
     const phone = document.querySelector(`#phone`);
     const monthly = document.querySelector(`#monthly`);
+    const type = document.querySelector(`#type`);
+    const ccName = document.querySelector(`#ccName`);
+    const ccNum = document.querySelector(`#ccNum`);
+    const month = document.querySelector(`#month`);
+    const year = document.querySelector(`#year`);
     const globalMsg = document.querySelector(`#globalMsg`);
     const errors = document.querySelector(`#errors`);
+    const cardInfo = document.querySelector(`#cardInfo`);
     let validated = false;
 
+    const checkUsername = function () {
+
+        if (!username.checkValidity()) {
+            username.setAttribute("aria-describedby", `#usernameInvalid`);
+            usernameFeedback.innerHTML = "Please provide a username.";
+        } else {
+            username.removeAttribute("aria-describedby");
+            usernameFeedback.innerHTML = "";
+        }
+    }
     const checkPasswords = function () {
         const passwordFeedback = document.querySelector(`#${pw1}Invalid`);
         const retypeFeedback = document.querySelector(`#${pw2}Invalid`);
@@ -55,10 +75,12 @@
         var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
         if (email.value == 0) {
             email.setCustomValidity('');
+            email.removeAttribute("aria-describedby");
         } else {
             if (email.value.match(mailformat)) {
                 email.setCustomValidity('');
             } else {
+                email.setAttribute("aria-describedby", `emailInvalid`);
                 email.setCustomValidity('Invalid email address');
             }
         }
@@ -87,6 +109,9 @@
             }
         }
     }
+    let checkDate = function () {
+
+    }
     // let notify = function () {
 
     // }
@@ -96,51 +121,61 @@
     //         globalMsg.innerHTML = "";
     //     }, 10000);
     // }
+    let togglePayment = function () {
+        if (monthly.checked) {
+            cardInfo.classList.add("show");
+        } else {
+            cardInfo.classList.remove("show");
+        }
+    }
 
-    //monthly.addEventListener('change', togglePayment);
+    monthly.addEventListener('change', togglePayment);
+
 
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
+                checkUsername();
                 checkPasswords();
+                //checkSocial();
                 checkEmail();
                 checkPhone();
+                //checkCC();
                 if (!form.checkValidity()) {
 
                     event.preventDefault()
                     event.stopPropagation()
                 } else {
-                    // if (monthly.checked) {
-                    //     let retVal = confirm("You're committing to paying $19.99 per month for membership dues.  Is that alright?  If so, click OK to continue.");
-                    //     if (retVal == true) {
-                    //         return true;
-                    //     } else {
-                    //         event.preventDefault()
-                    //         event.stopPropagation()
-                    //     }
-                    // }
+                    if (monthly.checked) {
+                        let retVal = confirm("You're committing to paying $19.99 per month for membership dues.  Is that alright?  If so, click OK to continue.");
+                        if (retVal == true) {
+                            return true;
+                        } else {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+                    }
                 }
 
                 var inv = form.querySelectorAll(`:invalid:not(fieldset)`), len = inv.length;
                 let errs = "";
                 for (var i = 0; i < len; i++) {
-                    console.log(inv[i]);
                     var controlId = inv[i].id;
-                    console.log(controlId);
                     var label = document.getElementById(controlId);
                     var labelText = accname.getAccessibleName(label);
                     var msg = inv[i].nextElementSibling.innerHTML;
-                    console.log(labelText);
-                    console.log(msg);
-                    errs += `<li><b><a href="#${controlId}">${labelText}</a>:</b><div>${msg}</div></li>`
+                    errs += `<li id="${controlId}Item"><b><a href="#${controlId}">${labelText}</a>:</b><div>${msg}</div></li>`
                 }
                 errors.innerHTML = errs;
 
                 if (!validated) {
                     form.classList.add('was-validated');
+                    username.addEventListener('input', checkUsername, false);
                     password.addEventListener('input', checkPasswords, false);
                     retype.addEventListener('input', checkPasswords, false);
+                    // ssn.addEventListener('input', checkSocial, false);
+                    // dob.addEventListener('input', checkDate, false);
                     email.addEventListener('input', checkEmail, false);
                     phone.addEventListener('input', checkPhone, false);
                     validated = true;
