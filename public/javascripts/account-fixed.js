cfef98d8d16b83320fd94f49760718087413daea
Rob Fentress <learn@vt.edu>
2022-02-22 09:35:15 -0500

    fix stuff

---
+++ public/javascripts/account-fixed.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('#search').on('submit', function (e) {
+    // validation code here
+    alert("Imagine this returns results.");
+    e.preventDefault();
+
+  });
+  $('.page-header a, .breadcrumb a, .page-sidebar a').click(function (e) {
+    alert("Imagine this takes you to a page.");
+    e.preventDefault();
+  });
+});
+
--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -1,22 +1,22 @@
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
 
     var mypass = document.getElementById("password");
-    console.log(accname.getAccessibleName(mypass));
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation');
     const pw1 = "password";
     const pw2 = "retype";
     const username = document.querySelector(`#username`);
-    const usernameFeedback = document.querySelector(`#usernameInvalid`);
+    const usernameFeedback = document.querySelector(`#usernameHelp`);
     const password = document.querySelector(`#${pw1}`);
     const retype = document.querySelector(`#${pw2}`);
     const email = document.querySelector(`#email`);
+    const emailFeedback = document.querySelector(`#emailHelp`);
     const ssn = document.querySelector(`#ssn`);
     const dob = document.querySelector(`#dob`);
+    const dobFeedback = document.querySelector(`#dobHelp`);
     const phone = document.querySelector(`#phone`);
     const monthly = document.querySelector(`#monthly`);
     const type = document.querySelector(`#type`);
@@ -25,26 +25,54 @@
     const month = document.querySelector(`#month`);
     const year = document.querySelector(`#year`);
     const globalMsg = document.querySelector(`#globalMsg`);
-    const errors = document.querySelector(`#errors`);
+    const errorRegion = document.querySelector(`#errorRegion`);
+    const errors = document.querySelector(`#errorsList`);
     const cardInfo = document.querySelector(`#cardInfo`);
+    let status = document.getElementById('status');
     let validated = false;
+
+    document.querySelectorAll('[data-hint]').forEach(item => {
+        let help = item.nextElementSibling;
+        help.innerHTML = item.getAttribute('data-hint');
+    });
+
+
+    const checkDOB = function () {
+        if (!dob.checkValidity()) {
+            dobFeedback.classList.add('invalid-help');
+            dobFeedback.innerHTML = "Date must be in format MM/DD/YYYY";
+        } else {
+            dobFeedback.classList.remove('invalid-help');
+            dobFeedback.innerHTML = "";
+            if (dob.getAttribute('data-hint')) {
+                dobFeedback.innerHTML = dob.getAttribute('data-hint');
+            }
+        }
+    }
 
     const checkUsername = function () {
 
         if (!username.checkValidity()) {
-            username.setAttribute("aria-describedby", `#usernameInvalid`);
-            usernameFeedback.innerHTML = "Please provide a username.";
+            usernameFeedback.classList.add('invalid-help');
+            if (username.value) {
+                usernameFeedback.innerHTML = "Contains a character that isn't a letter or number. Only alphanumeric characters allowed.";
+            } else {
+                usernameFeedback.innerHTML = "Username cannot be empty.";
+            }
         } else {
-            username.removeAttribute("aria-describedby");
+            usernameFeedback.classList.remove('invalid-help');
             usernameFeedback.innerHTML = "";
+            if (username.getAttribute('data-hint')) {
+                usernameFeedback.innerHTML = username.getAttribute('data-hint');
+            }
         }
     }
     const checkPasswords = function () {
-        const passwordFeedback = document.querySelector(`#${pw1}Invalid`);
-        const retypeFeedback = document.querySelector(`#${pw2}Invalid`);
+        let passwordFeedback = document.querySelector(`#${pw1}Help`);
+        let retypeFeedback = document.querySelector(`#${pw2}Help`);
         let pMsg = [], rMsg = [];
-        let missing = `Password missing`;
-        let different = `Passwords do not match`
+        let missing = `Password missing.`;
+        let different = `Passwords do not match.`
 
         if (password.validity.valueMissing) {
             pMsg.push(missing);
@@ -58,30 +86,44 @@
         }
         passwordFeedback.innerHTML = pMsg.join(`<br>`);
         retypeFeedback.innerHTML = rMsg.join(`<br>`);
-        if (pMsg !== "") {
-            password.setAttribute("aria-describedby", `${pw1}Invalid`);
-        } else {
-            password.removeAttribute("aria-describedby");
-        }
-        if (rMsg !== "") {
-            retype.setAttribute("aria-describedby", `${pw2}Invalid`);
-        } else {
-            retype.removeAttribute("aria-describedby");
-        }
         password.setCustomValidity(pMsg.join(` and `));
         retype.setCustomValidity(rMsg.join(` and `));
+
+        if (!password.checkValidity()) {
+            passwordFeedback.classList.add('invalid-help');
+        } else {
+            passwordFeedback.classList.remove('invalid-help');
+            if (retype.getAttribute('data-hint')) {
+                retypeFeedback.innerHTML = retype.getAttribute('data-hint');
+            }
+        }
+
+        if (!retype.checkValidity()) {
+            retypeFeedback.classList.add('invalid-help');
+        } else {
+            retypeFeedback.classList.remove('invalid-help');
+            if (password.getAttribute('data-hint')) {
+                passwordFeedback.innerHTML = password.getAttribute('data-hint');
+                console.log(password.getAttribute('data-hint'))
+            }
+        }
+
     }
     const checkEmail = function () {
         var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
         if (email.value == 0) {
             email.setCustomValidity('');
-            email.removeAttribute("aria-describedby");
+            emailFeedback.classList.remove('invalid-help');
         } else {
             if (email.value.match(mailformat)) {
                 email.setCustomValidity('');
+                emailFeedback.classList.remove('invalid-help');
+                if (email.getAttribute('data-hint')) {
+                    emailFeedback.innerHTML = email.getAttribute('data-hint');
+                }
             } else {
-                email.setAttribute("aria-describedby", `emailInvalid`);
-                email.setCustomValidity('Invalid email address');
+                email.setCustomValidity('Invalid email address. An example of a valid email would be: someone@someserver.com');
+                emailFeedback.classList.add('invalid-help');
             }
         }
     }
@@ -137,12 +179,28 @@
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
                 checkUsername();
+                checkDOB();
                 checkPasswords();
                 //checkSocial();
                 checkEmail();
                 checkPhone();
                 //checkCC();
                 if (!form.checkValidity()) {
+                    let numErrors = document.querySelectorAll('.invalid-help').length;
+                    status.innerHTML = `<div class="alert alert-danger">
+                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="images/bootstrap-icons.svg#exclamation-triangle-fill"/></svg>    
+                    ${numErrors} invalid form entries</div>
+                    `;
+
+
+                    if (numErrors > 0) {
+                        errorRegion.classList.remove("d-none");
+                    } else {
+                        errorRegion.classList.add("d-none");
+                    }
+                    document.querySelectorAll('.num-errors').forEach(item => {
+                        item.innerHTML = numErrors;
+                    });
 
                     event.preventDefault()
                     event.stopPropagation()
@@ -172,6 +230,7 @@
                 if (!validated) {
                     form.classList.add('was-validated');
                     username.addEventListener('input', checkUsername, false);
+                    dob.addEventListener('input', checkDOB, false);
                     password.addEventListener('input', checkPasswords, false);
                     retype.addEventListener('input', checkPasswords, false);
                     // ssn.addEventListener('input', checkSocial, false);
